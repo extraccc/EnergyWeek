118153f8b093fc43f2c98c698ef9e3a68ec678c7
steingeist <xylinchem@gmail.com>
2018-11-02 23:38:18 -0400

    add CZTS

--- main.js
+++ main.js
@@ -21,26 +21,6 @@ new Vue({
   data: {
     selected: null,
     materials: [{
-        name: "Si",
-        displayName: "silicon",
-        description: "Silicon is a chemical element with symbol Si and atomic number 14. It is a hard and brittle crystalline solid with a blue-grey metallic lustre; and it is a tetravalent metalloid and semiconductor. It is a member of group 14 in the periodic table: carbon is above it; and germanium, tin, and lead are below it. It is relatively unreactive.",
-        source: "https://en.wikipedia.org/wiki/Silicon",
-        geometryURL: "geometry/Si_DIA.in",
-        imageURL: "https://upload.wikimedia.org/wikipedia/commons/e/e9/SiliconCroda.jpg",
-        bandStructure: "band/Si_DIA.jpg",
-        reasons: "Perhaps yes."
-      },
-      {
-        name: "SiO2",
-        displayName: "silicon dioxide",
-        description: "Silicon dioxide, also known as silica, silicic acid or silicic acid anydride is an oxide of silicon with the chemical formula SiO₂, most commonly found in nature as quartz and in various living organisms. In many parts of the world, silica is the major constituent of sand.",
-        source: "https://en.wikipedia.org/wiki/Silicon_dioxide",
-        geometryURL: "geometry/SiO2_quartz.cif",
-        imageURL: "https://upload.wikimedia.org/wikipedia/commons/5/5f/Sample_of_silicon_dioxide.jpg",
-        bandStructure: "#",
-        reasons: "#"
-      },
-      {
         name: "C",
         displayName: "diamond",
         description: "Diamond is a solid form of carbon with a diamond cubic crystal structure. At room temperature and pressure it is metastable and graphite is the stable form, but diamond almost never converts to graphite. ",
@@ -49,15 +29,6 @@ new Vue({
         imageURL: "https://upload.wikimedia.org/wikipedia/commons/d/d7/Rough_diamond.jpg",
         bandStructure: "band/C_DIA.jpg",
         reasons: "Absolutely not! The gap is large, what's more, it is expensive."
-      }, {
-        name: "GaAs",
-        displayName: "gallium arsenide",
-        description: "Gallium arsenide is a compound of the elements gallium and arsenic. It is a III-V direct bandgap semiconductor with a Zinc blende crystal structure.",
-        source: "https://en.wikipedia.org/wiki/Gallium_arsenide",
-        geometryURL: "geometry/GaAs.in",
-        imageURL: "https://upload.wikimedia.org/wikipedia/commons/2/22/Gallium_Arsenide_%28GaAs%29_2%22_wafer.jpg",
-        bandStructure: "band/GaAs.jpg",
-        reasons: "Perhaps..."
       }, {
         name: "Graphene",
         displayName: "graphene",
@@ -79,13 +50,61 @@ new Vue({
         reasons: ""
       },
       {
-        name: "CBTSSe",
-        displayName: "CBTSSe",
+        name: "Si",
+        displayName: "silicon",
+        description: "Silicon is a chemical element with symbol Si and atomic number 14. It is a hard and brittle crystalline solid with a blue-grey metallic lustre; and it is a tetravalent metalloid and semiconductor. It is a member of group 14 in the periodic table: carbon is above it; and germanium, tin, and lead are below it. It is relatively unreactive.",
+        source: "https://en.wikipedia.org/wiki/Silicon",
+        geometryURL: "geometry/Si_DIA.in",
+        imageURL: "https://upload.wikimedia.org/wikipedia/commons/e/e9/SiliconCroda.jpg",
+        bandStructure: "band/Si_DIA.jpg",
+        reasons: "Perhaps yes."
+      },
+      {
+        name: "SiO2",
+        displayName: "silicon dioxide",
+        description: "Silicon dioxide, also known as silica, silicic acid or silicic acid anydride is an oxide of silicon with the chemical formula SiO₂, most commonly found in nature as quartz and in various living organisms. In many parts of the world, silica is the major constituent of sand.",
+        source: "https://en.wikipedia.org/wiki/Silicon_dioxide",
+        geometryURL: "geometry/SiO2_quartz.cif",
+        imageURL: "https://upload.wikimedia.org/wikipedia/commons/5/5f/Sample_of_silicon_dioxide.jpg",
+        bandStructure: "#",
+        reasons: "#"
+      },
+      {
+        name: "GaAs",
+        displayName: "gallium arsenide",
+        description: "Gallium arsenide is a compound of the elements gallium and arsenic. It is a III-V direct bandgap semiconductor with a Zinc blende crystal structure.",
+        source: "https://en.wikipedia.org/wiki/Gallium_arsenide",
+        geometryURL: "geometry/GaAs.in",
+        imageURL: "https://upload.wikimedia.org/wikipedia/commons/2/22/Gallium_Arsenide_%28GaAs%29_2%22_wafer.jpg",
+        bandStructure: "band/GaAs.jpg",
+        reasons: "Perhaps..."
+      },
+      {
+        name: "CBTS",
+        displayName: "CBTS",
+        description: "CBTS",
+        geometryURL: "geometry/CBTS.in",
+        imageURL: "#",
+        bandStructure: "band/CBTS.jpg",
+        reasons: "undefined"
+      },
+      {
+        name: "CBTSe",
+        displayName: "CBTSe",
         description: "will be widely used in the futrue(I guess)",
         geometryURL: "geometry/CBTSe.in",
         imageURL: "#",
         bandStructure: "band/CBTSe.jpg",
         reasons: "Perhaps.."
+      },
+      {
+        name: "CZTS",
+        displayName: "CZTS",
+        description: "CZTS",
+        geometryURL: "geometry/CZTS.in",
+        imageURL: "#",
+        bandStructure: "band/CZTS.jpg",
+        reasons: "undefined"
       },
       {
         name: "perovskite",
@@ -95,15 +114,6 @@ new Vue({
         imageURL: "#",
         bandStructure: "band/AE4TPbI4.jpg",
         reasons: "Perhaps yes..."
-      },
-      {
-        name: "CBTS",
-        displayName: "CBTS",
-        description: "AE4TPbI4",
-        geometryURL: "geometry/CBTS.in",
-        imageURL: "#",
-        bandStructure: "band/CBTS.jpg",
-        reasons: "undefined"
       }
     ]
   },
